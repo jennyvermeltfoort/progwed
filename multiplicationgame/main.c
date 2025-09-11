d937bdb299cd880e4ee2cef3a51bf0fb6ae32b61
Jenny Vermeltfoort <jennyvermeltfoort@outlook.com>
2025-09-11 16:36:06 +0200

    idk

--- multiplicationgame/main.c
+++ multiplicationgame/main.c
@@ -12,15 +12,44 @@ wanneer het aantal getallen even of oneven is? werkt niet werkt alleen voor een
 bepaalde n.
 
 n = 2 stan wint
-... n = 9 stan
+... n = 9 stan (9) --> zet 1
 n = 10 ollie wint
-... n = 18 ollie wint
+... n = 18 ollie wint (9 * 2) --> zet 2, de twee de minimale gooi van stan
 n = 19 stan wint
 ... n = 36 -> wanneer stan begint met een 2
 ... n = 54 -> wanneer stan begint met een 3 -> bij 28, stan begint, 3x9 = 18 max
                 dus 3x2x5 -> stan wint
 ... n = 82
-... n = 162 stan wint,
-n = 163 ollie wint,
-... n = ??? olie wint
+... n = 162 stan wint, (9 * 2 * 9) --> zet 3, de twee id de minimal gooi van
+ollie n = 163 ollie wint, (9 * 2 * 9 * 2)
+... n = 1458 olie wint (9 * 2 * 9 * 9) --> zet 4
+...
+
+
 */
+
+#include <stdio.h>
+
+int main(void) {
+    int n = 0;
+
+    while (scanf("%i\n", &n) > 0) {
+        int p = 0;
+        if (n > 10) {
+            float t = (float)n / 2.0f;
+            while (t > 1.0f) {
+                p++;
+                t = t / 9.0f;
+            }
+        }
+        printf("%i ", p);
+
+        if (p % 2) {
+            printf("Ollie wins.\n");
+        } else {
+            printf("Stan wins.\n");
+        }
+    }
+
+    return 0;
+}
